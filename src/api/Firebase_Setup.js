2cc7ed8b8b6bd5366bc8985c4604cede451c4c40
tixwho <905002146tix@gmail.com>
2021-11-05 16:44:43 -0500

    normalize naming & naming guides

--- src/api/Firebase_Setup.js
+++ src/api/Firebase_Setup.js
@@ -2,6 +2,9 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { firebaseConfig } from "../config/Firebase_config";
 
+//for pure JS file, Naming: Upper_case_first.js
+//for vue file, Naming: AlwaysUpperFirst.vue
+
 const firebaseApp = initializeApp(firebaseConfig);
 
 const firebaseDb = getFirestore();
